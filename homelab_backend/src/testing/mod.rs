98f2adfaa85f505b7f9682b1d96878b4bb9a94e6
kyle o'neill <kyleoneill24@gmail.com>
2024-06-08 15:56:58 -0400

    Pull common test code out of the user test file

--- homelab_backend/src/testing/mod.rs
+++ homelab_backend/src/testing/mod.rs
@@ -1,1 +1,61 @@
 mod user_testing;
+
+use crate::generate_app;
+use axum::body::Body;
+use hyper_util::client::legacy::connect::HttpConnector;
+use hyper_util::client::legacy::Client;
+use serde::Serialize;
+use sqlx::sqlite::SqlitePool;
+use std::net::SocketAddr;
+use std::sync::OnceLock;
+use tokio::net::TcpListener;
+
+#[allow(dead_code)]
+pub static SERVER: OnceLock<Client<HttpConnector, Body>> = OnceLock::new();
+#[allow(dead_code)]
+pub static ADDR: OnceLock<SocketAddr> = OnceLock::new();
+
+#[allow(dead_code)]
+pub async fn setup_database() {
+    let database_url = dotenv!("DATABASE_URL").to_owned();
+    let pool = SqlitePool::connect(database_url.as_str())
+        .await
+        .expect("Failed to connect to database");
+    let _ = sqlx::query!(
+            "\
+            DELETE FROM users;\
+            UPDATE `main`.`sqlite_sequence` SET `seq` = '0' WHERE  `name` = 'users';\
+            INSERT INTO users (username, password, auth_level, salt) VALUES ('admin', 'D600AD1AAEA6261F2B5923FE076AE08B42688CDF6051FEF2D8CC4ED303D19E22', 'Admin', 'zTGNpsiiXQ5f');\
+            "
+        ).execute(&pool).await;
+}
+
+#[allow(dead_code)]
+pub async fn initialize() {
+    setup_database().await;
+    match SERVER.get() {
+        Some(_val) => (),
+        None => {
+            let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+            let addr = listener.local_addr().unwrap();
+            // TODO: Might want to store this JoinHandle from spawn() so I can clean up resources
+            //       correctly at the end of testing?
+            tokio::spawn(async move { axum::serve(listener, generate_app().await).await.unwrap() });
+            let client =
+                hyper_util::client::legacy::Client::builder(hyper_util::rt::TokioExecutor::new())
+                    .build_http();
+            SERVER.set(client).unwrap();
+            ADDR.set(addr).unwrap();
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn json_bytes<T>(structure: T) -> Vec<u8>
+where
+    T: Serialize,
+{
+    let mut bytes: Vec<u8> = Vec::new();
+    serde_json::to_writer(&mut bytes, &structure).unwrap();
+    bytes
+}
--- homelab_backend/src/testing/user_testing.rs
+++ homelab_backend/src/testing/user_testing.rs
@@ -1,68 +1,14 @@
 #[cfg(test)]
 mod user_testing {
     use super::*;
-    use crate::generate_app;
     use axum::{
         body::Body,
         http::{Request, StatusCode},
     };
     use http_body_util::BodyExt;
-    use hyper_util::client::legacy::connect::HttpConnector;
-    use hyper_util::client::legacy::Client;
-    use serde::Serialize;
     use serde_json::json;
-    use sqlx::sqlite::SqlitePool;
-    use std::net::SocketAddr;
-    use std::sync::OnceLock;
-    use tokio::net::TcpListener;
 
-    static SERVER: OnceLock<Client<HttpConnector, Body>> = OnceLock::new();
-    static ADDR: OnceLock<SocketAddr> = OnceLock::new();
-
-    async fn setup_database() {
-        let database_url = dotenv!("DATABASE_URL").to_owned();
-        let pool = SqlitePool::connect(database_url.as_str())
-            .await
-            .expect("Failed to connect to database");
-        let _ = sqlx::query!(
-            "\
-            DELETE FROM users;\
-            UPDATE `main`.`sqlite_sequence` SET `seq` = '0' WHERE  `name` = 'users';\
-            INSERT INTO users (username, password, auth_level, salt) VALUES ('admin', 'D600AD1AAEA6261F2B5923FE076AE08B42688CDF6051FEF2D8CC4ED303D19E22', 'Admin', 'zTGNpsiiXQ5f');\
-            "
-        ).execute(&pool).await;
-    }
-
-    async fn initialize() {
-        setup_database().await;
-        match SERVER.get() {
-            Some(_val) => (),
-            None => {
-                let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-                let addr = listener.local_addr().unwrap();
-                // TODO: Might want to store this JoinHandle from spawn() so I can clean up resources
-                //       correctly at the end of testing?
-                tokio::spawn(
-                    async move { axum::serve(listener, generate_app().await).await.unwrap() },
-                );
-                let client = hyper_util::client::legacy::Client::builder(
-                    hyper_util::rt::TokioExecutor::new(),
-                )
-                .build_http();
-                SERVER.set(client).unwrap();
-                ADDR.set(addr).unwrap();
-            }
-        }
-    }
-
-    fn json_bytes<T>(structure: T) -> Vec<u8>
-    where
-        T: Serialize,
-    {
-        let mut bytes: Vec<u8> = Vec::new();
-        serde_json::to_writer(&mut bytes, &structure).unwrap();
-        bytes
-    }
+    use crate::testing::{initialize, json_bytes, ADDR, SERVER};
 
     #[tokio::test]
     async fn user_crud() {
