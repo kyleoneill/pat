14e240d62d60154869c8fc51c588719e64f2a48d
kyle o'neill <kyleoneill24@gmail.com>
2025-03-29 15:28:10 -0400

    Connection game is now playable

--- homelab-frontend/src/api/games_api.ts
+++ homelab-frontend/src/api/games_api.ts
@@ -1,20 +1,23 @@
 import axios from 'axios';
 
 import type { CreateConnectionsGame } from '@/models/games_interfaces';
-import type { RouteParamValue } from 'vue-router'
 
 export async function createConnectionsGame(gameData: CreateConnectionsGame) {
-  return await axios.post("/games/connections", gameData)
+  return await axios.post("/games/connections", gameData);
 }
 
 export async function getAllConnectionGamesForOthers() {
-  return await axios.get("/games/connections")
+  return await axios.get("/games/connections");
 }
 
 export async function getAllConnectionGamesForMe() {
-  return await axios.get("/games/connections/mine")
+  return await axios.get("/games/connections/mine");
 }
 
 export async function getConnectionGameToPlay(gameSlug: string) {
-  return await axios.get(`/games/connections/play/${gameSlug}`)
+  return await axios.get(`/games/connections/play/${gameSlug}`);
+}
+
+export async function trySolveConnectionGameRow(gameSlug: string, data: Array<string>) {
+  return await axios.put(`/games/connections/play/${gameSlug}/try_solve`, data);
 }
